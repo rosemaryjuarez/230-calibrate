f044ccef511e5de7f532942655e50878df4d402d
Vanessa-Salgado <vsalgadozavaleta@umail.ucsb.edu>
2024-04-29 21:33:00 -0700

    computed max

--- R/compute_highflowmetrics.R
+++ R/compute_highflowmetrics.R
@@ -6,27 +6,27 @@
 #' @param  month month
 #' @param  day day
 #' @param  year year
-#' @param low_flow_months which to use default (April - Month 4)
-#' @return annual_min_err, annual_min_corr, low_month_cor, low_month_err
+#' @param high_flow_months which to use default (April 4)
+#' @return annual_max_err, annual_max_corr, high_month_cor, high_month_err
 
-compute_highflowmetrics = function(m,o, month, day, year,wy, low_flow_months=4) {
+compute_highflowmetrics = function(m,o, month, day, year,wy, high_flow_months=4) {
   
   flow = cbind.data.frame(m,o, month, day, year,wy)
-  # first lets get minimum yearly values
+  # first lets get max yearly values
   
-  tmp = flow %>% group_by(wy) %>% summarize(mino=min(o), minm=min(m))
+  tmp = flow %>% group_by(wy) %>% summarize(maxo=max(o), maxm=max(m))
   
-  annual_min_err = mean(tmp$minm-tmp$mino)
+  annual_max_err = mean(tmp$maxm-tmp$maxo)
   
-  annual_min_cor = cor(tmp$minm, tmp$mino)
+  annual_max_cor = cor(tmp$maxm, tmp$maxo)
   
   # now lets get monthly values
   tmp = flow %>% group_by(month, year) %>% summarize(model=sum(m), obs=sum(o))
-  # now extract august
-  low = subset(tmp, month %in% low_flow_months)
-  low_month_err = mean(low$model-low$obs)
-  low_month_cor=cor(low$model, low$obs)
+  # now extract march
+  high = subset(tmp, month %in% high_flow_months)
+  high_month_err = mean(high$model-high$obs)
+  high_month_cor=cor(high$model, high$obs)
   
-  return(list(annual_min_err=annual_min_err, annual_min_cor=annual_min_cor, low_month_err=low_month_err,
-              low_month_cor=low_month_cor))
+  return(list(annual_max_err=annual_max_err, annual_max_cor=annual_max_cor, high_month_err=high_month_err,
+              high_month_cor=high_month_cor))
 }
--- R/compute_highflowmetrics_all.R
+++ R/compute_highflowmetrics_all.R
@@ -1,4 +1,4 @@
-#' highlowmetrics
+#' highflowmetrics
 #'
 #' Compute percent error between observation and model
 #' @param  m  model estimates
@@ -6,49 +6,49 @@
 #' @param  month month
 #' @param  day day
 #' @param  year year
-#' @param low_flow_months which to use default (April - Month 4)
-#' @param wts (vector of 4 for annual_min_err, annual_min_corr, low_month_cor, low_month_err)
-#' @param max_err_annual_min
-#' @param max_err_low_month
+#' @param high_flow_months (April 4)
+#' @param wts (vector of 4 for annual_max_err, annual_max_corr, high_month_cor, high_month_err)
+#' @param max_err_annual_max
+#' @param max_err_high_month
 #' @return annual_min_err, annual_min_corr, low_month_cor, low_month_err
 
 
-compute_lowflowmetrics_all = function(m,o, month, day, year,wy, low_flow_months=4,
-                                      max_err_annual_min=NULL, max_err_low_month=NULL, wts=c(0.25,0.25,0.25,0.25)) {
+compute_highflowmetrics_all = function(m,o, month, day, year,wy, high_flow_months=4,
+                                       max_err_annual_max=NULL, max_err_high_month=NULL, wts=c(0.25,0.25,0.25,0.25)) {
   
   
   flow = cbind.data.frame(m,o, month, day, year,wy)
   # first lets get minimum yearly values
   
-  tmp = flow %>% group_by(wy) %>% dplyr::summarize(mino=min(o), minm=min(m))
+  tmp = flow %>% group_by(wy) %>% dplyr::summarize(maxo=max(o), maxm=max(m))
   
-  annual_min_err = mean(tmp$minm-tmp$mino)
+  annual_max_err = mean(tmp$maxm-tmp$maxo)
   
-  annual_min_cor = cor(tmp$minm, tmp$mino)
+  annual_max_cor = cor(tmp$maxm, tmp$maxo)
   
-  if (is.null(max_err_annual_min))
-  { max_err_annual_min = 0.5*mean(tmp$mino)}
+  if (is.null(max_err_annual_max))
+  { max_err_annual_max = 0.5*mean(tmp$maxo)}
   
   # now lets get monthly values
   tmp = flow %>% group_by(month, year) %>% dplyr::summarize(model=sum(m), obs=sum(o))
   # now extract august
-  low = subset(tmp, month %in% low_flow_months)
-  low_month_err = mean(low$model-low$obs)
-  low_month_cor=cor(low$model, low$obs)
+  high = subset(tmp, month %in% high_flow_months)
+  high_month_err = mean(high$model-high$obs)
+  high_month_cor=cor(high$model, high$obs)
   
   # if user doesn't specify maximum errors use 50% of mean observed values
-  if (is.null(max_err_low_month))
-  { max_err_low_month = 0.5*mean(low$obs)}
+  if (is.null(max_err_high_month))
+  { max_err_high_month = 0.5*mean(high$obs)}
   
-  annual_min_err_trans = max(0,(1-abs(annual_min_err/max_err_annual_min) ))
-  low_month_err_trans = max(0,(1-abs(low_month_err/max_err_low_month) ))
+  annual_max_err_trans = max(0,(1-abs(annual_max_err/max_err_annual_max) ))
+  high_month_err_trans = max(0,(1-abs(high_month_err/max_err_high_month) ))
   
   # apply weight (normalize in case they don't sum to 1)
   wts = wts/sum(wts)
   
-  combined = wts[1]*annual_min_err_trans + wts[2]*annual_min_cor+
-    wts[3]*low_month_cor + wts[4]*low_month_err_trans
+  combined = wts[1]*annual_max_err_trans + wts[2]*annual_max_cor+
+    wts[3]*high_month_cor + wts[4]*high_month_err_trans
   
-  return(list(annual_min_err=annual_min_err, annual_min_cor=annual_min_cor, low_month_err=low_month_err,
-              low_month_cor=low_month_cor, combined=combined))
+  return(list(annual_max_err=annual_max_err, annual_max_cor=annual_max_cor, high_month_err=high_month_err,
+              high_month_cor=high_month_cor, combined=combined))
 }
